39c65d92a214f06876756c8952e86259425215cb
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-01-25 02:13:26 -0300

    Nota Aluno

---
+++ src/ifNota/NotaAluno.java
@@ -0,0 +1,33 @@
+package ifNota;
+
+
+import java.util.Scanner;
+
+public class NotaAluno {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Digite uma nota:");
+		double nota = input.nextDouble();
+
+		if (nota > 10 || nota < 0) {
+			System.out.println("Nota inválida");
+		} else if (nota >= 8.1) {
+			System.out.println("Conceito A");
+			System.out.println("Parabéns!!!");
+		} else if (nota >= 6.1) {
+			System.out.println("Conceito B");
+		} else if (nota >= 4.1) {
+			System.out.println("Conceito C");
+		} else if (nota >= 2.1) {
+			System.out.println("Conceito D");
+		} else {
+			System.out.println("Conceito E");
+		}
+		System.out.println("Fim");
+		input.close();
+	}
+
+}
